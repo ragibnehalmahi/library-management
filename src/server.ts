b8b701fdee6b20d2c3433bbfc8a049fabf259b66
Ragib Nehal Mahi <76880336+ragibnehalmahi@users.noreply.github.com>
2025-07-12 11:00:55 +0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,31 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import express from 'express';
 import app from './app';
+import bookRoutes from './routes/book.route';
+import borrowRoutes from './routes/borrow.route';
 
+ 
 dotenv.config();
+
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGODB_URI as string;
+ 
+ 
 
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
+ const startServer = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
     console.log('✅ MongoDB connected');
-    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-  })
-  .catch(err => console.error('❌ MongoDB connection error:', err));
+    
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error('❌ MongoDB connection error:', err);
+    process.exit(1);
+  }
+};
+
+startServer();
