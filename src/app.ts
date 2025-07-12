ce4544697d22109031cf106581ea7316c8e138ed
Ragib Nehal Mahi <76880336+ragibnehalmahi@users.noreply.github.com>
2025-07-12 11:00:25 +0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import express from 'express';
 import bookRouter from './routes/book.route';
 import borrowRouter from './routes/borrow.route';
-
+import cors from 'cors'
 const app = express();
+
+app.use(cors({
+  origin:[' http://localhost:5173','https://library-management-frontend-8ont.vercel.app/api'],
+  
+  
+   
+}));
+
 app.use(express.json());
 app.use('/api/books', bookRouter);
 app.use('/api/borrow', borrowRouter);
